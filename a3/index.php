651cc00e5c709a7220b88a9004b95190e59c4c90
s3481151 <s1234567@student.rmit.edu.au>
2019-09-27 00:34:51 +1000

    Navigation function added

---
+++ a3/index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assignment 2</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
+    <script src='../wireframe.js'></script>
+</head>
+
+<body>
+    <header class="header">
+        <div class="inner-header">
+            <div class="logo-container">
+                <img class="logo-image" src="../../media/cinema-logo.jpg" alt="Cinema Logo" />
+                <h1>My Cinema <span>Website</span></h1>
+            </div>
+        </div>
+    </header>
+    <nav class="navigation-container">
+        <ul class="navigation">
+            <a class="navLink" href="#about">
+                <li>About US</li>
+            </a>
+            <a class="navLink" href="#prices">
+                <li>Prices</li>
+            </a>
+            <a class="navLink" href="#showing">
+                <li>Now Showing</li>
+            </a>
+        </ul>
+    </nav>
+
+    <section id="about" class="content-container">
+        <div class="content-section"></div>
+        <div class="content-stuff">
+            <p>With our grand reopening, we have improved our theatres by incorporating the new 3D Dolby Vision
+                projection and
+                Dolby Atmos sound systems:</p>
+            <img src="../../media/mtk-image.jpg" alt="" class="mkt-image" />
+        </div>
+        <div class="content-section"></div>
+        <div class="content-stuff">
+            <p> We also improved our seating arraignments for both the Standard and First Class seats:</p>
+            <p>Standard Seats</p>
+            <img src="../../media/normal-chairs.jpg" alt="" class="mkt-image" />
+
+        </div>
+        <div class="content-section"></div>
+        <div class="content-stuff">
+            <p>First Class Seats</p>
+            <img src="../../media/Verona-Twin.png" alt="" class="mkt-image" />
+            <img src="../../media/firstclass-person.png" alt="" class="mkt-image" />
+        </div>
+        <div class="content-section"></div>
+    </section>
+
+
+    <section id="prices" class="price-container">
+        <h1>Prices</h1>
+        <div class="content-stuff">
+
+            <h2 class="price-main">Standard Adult STA</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 14.00</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 19.80</p>
+            <p class="text-muted"></p>
+        </div>
+        <div class="content-stuff">
+            <h2 class="price-main">Standard Concession STP</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 12.50</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 17.50</p>
+            <p class="text-muted"></p>
+        </div>
+        <div class="content-stuff">
+            <h2 class="price-main">Standard Child STC</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 11.00</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 15.30</p>
+            <p class="text-muted"></p>
+        </div>
+        <div class="content-stuff">
+            <h2 class="price-main">First Class Adult FCA</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 24.00</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 30.00</p>
+            <p class="text-muted"></p>
+        </div>
+        <div class="content-stuff">
+            <h2 class="price-main">First Class Concession FCP</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 22.50</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 27.00</p>
+            <p class="text-muted"></p>
+        </div>
+        <div class="content-stuff">
+            <h2 class="price-main">First Class Child FCC</h2>
+            <p class="price-subtitle">Monday, Wednesday (ALL DAY) + 12PM other Weekdays</p>
+            <p class="price-number">AUD 21.00</p>
+            <p class="price-subtitle">All Other Times</p>
+            <p class="price-number">AUD 24.00</p>
+            <p class="text-muted"></p>
+        </div>
+
+
+
+    </section>
+
+    <section id="showing" class="content-container">
+
+        <div class="row">
+
+            <div class="column">
+
+                <div class="card">
+
+                    <div class="row">
+
+                        <div class="column">
+                            <img src='../../media/avengers.jpg' alt='Avengers poster' height=240 />
+                        </div>
+
+                        <div class="column">
+
+                            <h3>Avengers: Endgame PG-13</h3>
+                            <p>Wed: 9pm</p>
+                            <p>Thu: 9pm</p>
+                            <p>Fri: 9pm</p>
+                            <p>Sat: 6pm</p>
+                            <p>Sun: 6pm</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="column">
+
+                <div class="card">
+
+                    <div class="row">
+
+                        <div class="column">
+                            <img src='../../media/top-end-wedding.jpg' alt='Top End Weddin poster' height=240 />
+                        </div>
+
+                        <div class="column">
+
+                            <h3>Top End Wedding M</h3>
+                            <p>Mon: 6pm</p>
+                            <p>Tue: 6pm</p>
+                            <p>Sat: 3pm</p>
+                            <p>Sun: 3pm</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+            <div class="column">
+
+                <div class="card">
+                    <div class="row">
+
+                        <div class="column">
+
+                            <img src='../../media/Dumbo.png' alt='Dumbo poster' height=240 />
+
+                        </div>
+
+                        <div class="column">
+
+                            <h3>Dumbo PG</h3>
+                            <p>Mon: 12pm</p>
+                            <p>Tue: 12pm</p>
+                            <p>Wed: 6pm</p>
+                            <p>Thu: 6pm</p>
+                            <p>Fri: 6pm</p>
+                            <p>Sat: 12pm</p>
+                            <p>Sun: 12pm</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="column">
+
+                <div class="card">
+                    <div class="row">
+
+                        <div class="column">
+
+                            <img src='../../media/The-Happy-Prince.jpg' alt='The Happy Prince poster' height=240 />
+
+                        </div>
+
+                        <div class="column">
+
+                            <h3>The Happy Prince R</h3>
+                            <p>Wed: 12pm</p>
+                            <p>Thu: 12pm</p>
+                            <p>Fri: 12pm</p>
+                            <p>Sat: 9pm</p>
+                            <p>Sun: 9pm</p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <br>
+        <br>
+
+
+
+        <div class="row">
+
+            <div class="full-column">
+
+                <div class="card">
+
+                    <div class="row">
+
+                        <div class="column">
+                            <h3>Avengers: Endgame PG-13</h3>
+                            <br>
+                            <h4 class="plot-description-h3">Plot description </h4>
+                            <br>
+                            <br>
+                            <p class="plot-description">After the devastating events of Avengers: Infinity War (2018),
+                                the universe is
+                                in ruins. With the help of remaining allies, the Avengers assemble once more in order to
+                                reverse Thanos'
+                                actions and restore balance to the universe.</p>
+                        </div>
+
+                        <div class="column">
+
+                            <!-- <iframe width="420" height="345" src="https://youtu.com/TcMBFSGVi1c"></iframe> -->
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/TcMBFSGVi1c"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="column">
+                            <h4>Mak a Booking: </h4>
+                        </div>
+
+                        <div class="column">
+                            <button class="button">Wed: 9pm</button>
+                            <button class="button">Thu: 9pm</button>
+                            <button class="button">Fri: 9pm</button>
+                            <button class="button">Sat: 6pm</button>
+                            <button class="button">Sun: 6pm</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <script type="text/javascript" src="script.js"></script>
+    </section>
+
+    <footer class="footer">
+        <div class="inner-footer">
+            <p>Lunardo Cinema phone: 0412345678 </p>
+            <p>Email: Lunardo@Cinema.net Address: Someplace Somewhere Vic</p>
+            <br>
+            <p>Zaid Soboh</p>
+            <p>Student ID: s3481151</p>
+            <p>s3481151@student.rmit.edu.au</p>
+            <p><a href="https://github.com/s3481151/wp" target="_blank"> GitHub repository</a></p>
+
+
+            <div>
+                <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+            </div>
+    </footer>
+    <!-- <script src="js/wireframe.js" async defer></script> -->
+</body>
+
+</html>
--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,48 @@
-/* Insert your javascript here */
+
+
+
+
+window.onscroll = function () {
+    var sections = this.document.querySelectorAll("section");
+    var navLinks = this.document.getElementsByClassName("navLink");
+    var n = -1;
+
+    function clearActiveNav() {
+        for (var i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove("active");
+        }
+    }
+
+    // var sections = this.document.querySelectorAll("section");
+
+    console.log(window.scrollY);
+    clearActiveNav()
+
+
+    
+    // makes sure that areas abouve or bellow the sections do not highlight the navbar
+    while (n < sections.length - 1 && sections[n + 1].offsetTop <= window.scrollY &&
+        (sections[sections.length - 1].offsetTop + sections[sections.length - 1].scrollHeight) > window.scrollY) {
+        
+        
+            
+        console.log("n  before incroment is: " + n);
+        n++;
+        console.log("n is: " + n);
+
+        if (n >= 0) {
+            clearActiveNav()
+            navLinks[n].classList.add("active");
+            //  console.log(sections[n+1].id + "(offsetTop): " + sections[n+1].offsetTop);
+            //  console.log("window.scrollY is: " + window.scrollY);
+        }
+
+        // if(n >= sections.length-1){
+        //     n=-1;
+        // }
+
+    }
+
+
+
+}
