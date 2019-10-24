2826adf4bbe2e3b1631d2642c0a7ff2f81fc40d1
s3481151 <s1234567@student.rmit.edu.au>
2019-10-25 02:01:02 +1100

    Testing invoice php

--- a4/invoice.php
+++ a4/invoice.php
@@ -1,11 +1,13 @@
 <?php
-echo this is test 1
+// echo this is test 1
 
-echo this is test 2;
+// echo this is test 2;
 
-echo "this is test 3"
+echo "this is test 3";
 
-echo "this is test 4";
+echo "this is test 4"
+
+echo "this is test 5"
 
 
 
