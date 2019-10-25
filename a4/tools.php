ccf3138c1e482d591f66089753da68b2bbbb684e
s3481151 <s1234567@student.rmit.edu.au>
2019-10-25 14:01:06 +1100

    Adding a function to tools.php

--- a4/tools.php
+++ a4/tools.php
@@ -1,5 +1,17 @@
 <?php
   session_start();
+  
+
+  function session_set_post() {
+
+    $_SESSION['movie'] = $_POST['movie'];
+    $_SESSION['seats'] = $_POST['seats'];
+    $_SESSION['cust'] = $_POST['cust'];
+
+}
+
+//TODO: might want to move this up if it can be before the function 
+session_set_post();
 
 // Put your PHP functions and modules here
 
